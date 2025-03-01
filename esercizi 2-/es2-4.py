69612dc57b81f4a3d3e65ec873624fc1772c3e57
derstefino <stefano.gay@outlook.it>
2025-03-01 17:00:14 +0100

    esercizi 2-

---
+++ esercizi 2-/es2-4.py
@@ -0,0 +1,12 @@
+'''2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.'''
+
+# name of the person
+personsName = "stefano"
+
+# prints of name in lower, upper and titlecase
+
+print(personsName.lower())
+
+print(personsName.upper())
+
+print(personsName.title())
