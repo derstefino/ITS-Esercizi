e8cc6c5f5d6b92497e9606f035b723c5e0afc191
derstefino <stefano.gay@outlook.it>
2025-04-07 22:17:12 +0200

    commit

--- eccezioni/2.py
+++ eccezioni/2.py
@@ -3,9 +3,32 @@ Minimum length of 20 characters, At least three uppercase letters, At least four
 If the password is valid, the function should return True.
 If the password is invalid, the function should raise a built-in exception (e.g., ValueError) with a message indicating which criteria were not met.
 '''
+import string
 
+def validate_password(password:str):
 
-def validate_password(password):
+    if len(password)<20:
+        raise ValueError("Password too short, must be at least 20 characters long")
+    
+    upper_count=0
+    non_alpha_count=0
 
-    try:
-        if len(password)>=20 and 
+    for c in password:
+        if c in list(string.ascii_uppercase):
+            upper_count+=1
+        if c in ["!","$","%","&","/","?",".","-","_"]:
+            non_alpha_count+=1
+
+    if upper_count<3:
+            raise ValueError("Password must have at least 3 uppercase letters")
+    if non_alpha_count<4:
+            raise ValueError("Password must have at least 4 non aplhanumerical characters")
+        
+    return True
+    
+
+print(validate_password("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhHHH"))
+        
+        
+    
+
